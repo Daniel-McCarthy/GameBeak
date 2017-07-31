c3c94e55e60fc487717427899933b3a8da6c6e6c
Dan McCarthy <wererabbbit@yahoo.com>
2017-07-31 14:31:09 -0500

    Experimental MBC3 Support Added
    -Base MBC3 support has been added. Currently the RTC clock is NOT fully
    supported. Therefore until it is, MBC3 will not be handled by
    writeMemory and loadRom.
    
    Theoretically MBC3 games that do not use the clock should function, but
    there's not a great plethora of MBC3 games as a whole.
    
    -Includes minor change to MBC2 which comments out some code that does
    not get used. It will likely be deleted later.

--- GameBeak/GameBeak/Memory.h
+++ GameBeak/GameBeak/Memory.h
@@ -247,6 +247,27 @@ class Memory
 		}
 	}
 
+	void changeMBC3RomBanks(int bankNumber)
+	{
+		if (bankNumber == 0)
+		{
+			bankNumber++;
+		}
+
+		if ((bankNumber >= 0) && (bankNumber <= 0x7F))
+		{
+
+			int bankAddress = 0x4000 * bankNumber;
+
+			int fixedBankAddress = 0x4000;
+			for (int i = 0; i < 0x4000; i++)
+			{
+				beakRam[fixedBankAddress + i] = beakRom[bankAddress + i];
+			}
+
+			romBankNumber = bankNumber;
+		}
+	}
 
 	void changeMBC5RomBanks(int bankNumber)
 	{
@@ -379,9 +400,93 @@ class Memory
 			}
 
 		}
+		/*
+		Should never get called, as MBC writes only occur if it is less than 0x7FFF
 		else if (address >= 0xA000 && address <= 0xA1FF)
 		{
 			//External Ram 512x4bits
+			beakRam[(unsigned short)address] = value;
+		}
+		*/
+	}
+
+	void writeMBC3Value(short address, byte value)
+	{
+		if (address >= 0x0000 && address <= 0x1FFF)
+		{
+			//Ram/RTC Register  Enable/Disable
+			if ((value & 0x0F) == 0x0A)
+			{
+				//Enable Ram
+				ramEnabled = true;
+			}
+			else
+			{
+				//Disable Ram
+				ramEnabled = false;
+			}
+		}
+		else if (address >= 0x2000 && address <= 0x3FFF)
+		{
+			//Set Rom Bank Number 7 bits
+			byte newBankNumber = (value & 0x7F);
+
+			if (romBankNumber != newBankNumber)
+			{
+				changeMBC3RomBanks(newBankNumber);
+			}
+
+		}
+		else if (address >= 0x4000 && address <= 0x5FFF)
+		{
+			//Set Ram Bank Number /OR/ RTC Register Select
+
+			if ((value >= 0) && (value <= 3))
+			{
+				//Change Ram Bank
+				byte newBankNumber = value & 0x03;
+
+				if (ramBankNumber != newBankNumber)
+				{
+					changeRamBanks(newBankNumber);
+				}
+			}
+
+			if ((value >= 8) && (value <= 0x0C))
+			{
+				//RTC Register Select
+
+				//Todo: Implement this
+			}
+
+		}
+		else if (address >= 0x6000 && address <= 0x7FFF)
+		{
+			//Latch Clock Write
+			//if ((previousRTCWrite == 0) && (value == 1))
+			if (value == 1)
+			{
+				//Update RTC registers
+
+				LPSYSTEMTIME time;
+				GetSystemTime(time);
+
+
+				/*
+				08h  RTC S   Seconds   0-59 (0-3Bh)
+				09h  RTC M	Minutes   0-59 (0-3Bh)
+				0Ah  RTC H	Hours     0-23 (0-17h)
+				0Bh  RTC DL	Lower 8 bits of Day Counter (0-FFh)
+				0Ch  RTC DH	Upper 1 bit of Day Counter, Carry Bit, Halt Flag
+				Bit 0	Most significant bit of Day Counter (Bit 8)
+				Bit 6	Halt (0=Active, 1=Stop Timer)
+				Bit 7	Day Counter Carry Bit (1=Counter Overflow)
+
+				The Halt Flag is supposed to be set before <writing> to the RTC Registers.
+				*/
+
+			}
+			//Todo: Implement this
 		}
 
 	}
