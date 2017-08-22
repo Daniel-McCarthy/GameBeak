0afbe024205013a0b6490af61381984822f5f4b1
Dan McCarthy <wererabbbit@yahoo.com>
2017-08-22 15:06:08 -0500

    Reorganized Color.h and Removed Unused Code

--- GameBeak/GameBeak/Color.h
+++ GameBeak/GameBeak/Color.h
@@ -14,24 +14,15 @@ public:
 	unsigned char g;
 	unsigned char b;
 	unsigned char a;
-
-	bool Color::operator!=(Color& operand);
-	void Color::operator*=(Color& operand);
+	int getInt();
 
 	Color();
 	Color(unsigned char r, unsigned char g, unsigned char b);
 	Color(unsigned char r, unsigned char g, unsigned char b, unsigned char a);
 
-	int getInt();
-
+	bool operator!=(Color& operand);
+	void operator*=(Color& operand);
 	bool operator==(Color& operand);
-
-	/*
-	const Color operator*(const Color& lhs, const Color& rhs)
-	{
-		return Color(lhs.r * rhs.r, rhs.g * rhs.g, lhs.b * rhs.b);
-	}
-	*/
 
 	friend Color operator*(Color& lhs, Color& rhs)
 	{
