0c0c121a7e58ee3836c4184144d104739a063fa0
Dan McCarthy <wererabbbit@yahoo.com>
2017-08-25 00:07:43 -0500

    Added AssemblyView Update Registers Function

--- GameBeak/GameBeak-Frontend/AssemblyView.cs
+++ GameBeak/GameBeak-Frontend/AssemblyView.cs
@@ -16,6 +16,30 @@ namespace GameBeak_Frontend
         {
             InitializeComponent();
         }
+
+        //Call this upon:
+        //Stepping
+        //Pausing
+        //Gaining Window Focus
+        //More?
+        void updateDisplayValues()
+        {
+            /*
+            int[] registerValues = new int[4];
+            Marshal.Copy(NativeMethods.getRegisters(), registerValues, 0, 4);
+
+            afValue.Text = registerValues[0].ToString();
+            bcValue.Text = registerValues[1].ToString();
+            deValue.Text = registerValues[2].ToString();
+            hlValue.Text = registerValues[3].ToString();
+            */
+
+            afValue.Text = NativeMethods.getAF().ToString("X4");
+            bcValue.Text = NativeMethods.getBC().ToString("X4");
+            deValue.Text = NativeMethods.getDE().ToString("X4");
+            hlValue.Text = NativeMethods.getHL().ToString("X4");
+        }
+
         void updatePCValues()
         {
             short pc = NativeMethods.getPC();
