10640825a05decc90dc738bde092cd7fd284b42d
Dan McCarthy <wererabbbit@yahoo.com>
2017-08-25 00:04:49 -0500

    Added Anchors to MainWindow Picture Box to allow Scaling

--- GameBeak/GameBeak-Frontend/MainWindow.Designer.cs
+++ GameBeak/GameBeak-Frontend/MainWindow.Designer.cs
@@ -88,6 +88,10 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Purple;
             this.pictureBox1.Location = new System.Drawing.Point(0, 27);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(320, 288);
