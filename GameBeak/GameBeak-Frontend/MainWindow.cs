fdec3a012a3b03ea9e4cd3f737f78d4a24ccd6cb
Dan McCarthy <wererabbbit@yahoo.com>
2017-08-25 00:16:42 -0500

    Added NativeMethods GameBeak C++ DLLImport Functions
    -A Namespace called NativeMethods has been created to include the
    DLLImport functions from the GameBeak C++ DLL functions.

--- GameBeak/GameBeak-Frontend/MainWindow.cs
+++ GameBeak/GameBeak-Frontend/MainWindow.cs
@@ -17,5 +17,48 @@ namespace GameBeak_Frontend
         {
             InitializeComponent();
         }
+
+    public class NativeMethods
+    {
+
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool getPauseState();
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void setPauseState(bool pauseEnabled);
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool getRunState();
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void setRunState(bool runEnabled);
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr getRegisters();
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern short getAF();
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern short getBC();
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern short getDE();
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern short getHL();
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr getStackValues();
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern short getPC();
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr getCurrentMemoryMap();
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr getScreenDimensions();
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr getScreenPixelData();
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void setRom(byte[] rom, int romSize);
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void setStep();
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern byte readMemoryByte(short address);
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void initiateEmulator();
+
+        [DllImport("GameBeak.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void disassembleAddress(ref short address, [MarshalAs(UnmanagedType.LPStr)] StringBuilder cString, ref int stringSize);
     }
 }
