cebcb75e3d4b05d43faf6ddd7a20760a344b10a7
Dan McCarthy <wererabbbit@yahoo.com>
2017-08-24 23:59:03 -0500

    Removed listBox default values
    Removed default values from listBox. These were only for display to show
    how it would look once disassembly was able to be displayed. Now that
    the functionality is in, these display values are no longer required.

--- GameBeak/GameBeak-Frontend/AssemblyView.Designer.cs
+++ GameBeak/GameBeak-Frontend/AssemblyView.Designer.cs
@@ -63,11 +63,6 @@
             this.listBox1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 18;
-            this.listBox1.Items.AddRange(new object[] {
-            "0000: Ld HL, 05",
-            "0001: Add DE, HL",
-            "0005: Dec H",
-            "000A: Add DE, HL"});
             this.listBox1.Location = new System.Drawing.Point(218, 12);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(302, 292);
