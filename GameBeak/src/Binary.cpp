b116edfaad71ac2bab3bc7ecba0a70f999f81cdf
Dan McCarthy <dan.willy.mccarthy@gmail.com>
2018-10-01 21:03:29 -0500

    Binary: Replaced uses of byte with unsigned char
    Unsigned char will no longer by typedef'd to byte. Therefore it is being changed to unsigned char for compatibility.

--- GameBeak/src/Binary.cpp
+++ GameBeak/src/Binary.cpp
@@ -1,158 +1,158 @@
 
 #include "Binary.h"
 
-byte rotateLeft(byte number)
+unsigned char rotateLeft(unsigned char number)
 {
-	byte lostBit = (number & 0x80) >> 7;
-	number <<= 1;
-	number |= lostBit;
+    unsigned char lostBit = (number & 0x80) >> 7;
+    number <<= 1;
+    number |= lostBit;
 
-	return number;
+    return number;
 }
 
-byte rotateLeft(byte number, int shiftAmount)
+unsigned char rotateLeft(unsigned char number, int shiftAmount)
 {
-	for (int i = 0; i < shiftAmount; i++)
-	{
-		number = rotateLeft(number);
-	}
+    for (int i = 0; i < shiftAmount; i++)
+    {
+        number = rotateLeft(number);
+    }
 
-	return number;
+    return number;
 }
 
-byte rotateRight(byte number)
+unsigned char rotateRight(unsigned char number)
 {
-	byte lostBit = (number & 0x01) << 7;
-	number >>= 1;
-	number |= lostBit;
+    unsigned char lostBit = (number & 0x01) << 7;
+    number >>= 1;
+    number |= lostBit;
 
-	return number;
+    return number;
 }
 
-byte rotateRight(byte number, int shiftAmount)
+unsigned char rotateRight(unsigned char number, int shiftAmount)
 {
-	for (int i = 0; i < shiftAmount; i++)
-	{
-		number = rotateRight(number);
-	}
+    for (int i = 0; i < shiftAmount; i++)
+    {
+        number = rotateRight(number);
+    }
 
-	return number;
+    return number;
 }
 
-byte returnHalfNibble(byte data, int halfNibblePosition)
+unsigned char returnHalfNibble(unsigned char data, int halfNibblePosition)
 {
-	//Returns 2 bits from byte.
-	//If halfNibble Position == 0, it will index 000000XX
-	//If halfNibble Position == 1, it will index 0000XX00
-	//If halfNibble Position == 2, it will index 00XX0000
-	//If halfNibble Position == 3, it will index XX000000
+    //Returns 2 bits from byte.
+    //If halfNibble Position == 0, it will index 000000XX
+    //If halfNibble Position == 1, it will index 0000XX00
+    //If halfNibble Position == 2, it will index 00XX0000
+    //If halfNibble Position == 3, it will index XX000000
 
-	return (data & (3 << (halfNibblePosition * 2))) >> (halfNibblePosition * 2);
+    return (data & (3 << (halfNibblePosition * 2))) >> (halfNibblePosition * 2);
 }
 
-byte reverseBits(byte number)
+unsigned char reverseBits(unsigned char number)
 {
-	number = ((number & 0xF0) >> 4) | ((number & 0x0F) << 4);
-	number = ((number & 0xCC) >> 2) | ((number & 0x33) << 2);
-	number = ((number & 0xAA) >> 1)  | ((number & 0x55) << 1);
-	return number;
+    number = ((number & 0xF0) >> 4) | ((number & 0x0F) << 4);
+    number = ((number & 0xCC) >> 2) | ((number & 0x33) << 2);
+    number = ((number & 0xAA) >> 1)  | ((number & 0x55) << 1);
+    return number;
 }
 
 std::string hexToASCII(int value)
 {
-	std::string output = "";
+    std::string output = "";
 
-	if (value > 0)
-	{
-		while (value > 0)
-		{
-			byte toConvert = value & 0x0F;
-			value >>= 4;
+    if (value > 0)
+    {
+        while (value > 0)
+        {
+            unsigned char toConvert = value & 0x0F;
+            value >>= 4;
 
-			if (toConvert > 9)
-			{
-				//0x41 is ASCII 'a', value of character will index appropriate letter
-				toConvert -= 0xA;
-				output = (char)(0x61 + toConvert) + output;
-			}
-			else
-			{
-				//0x30 is ASCII '0', value of number will index appropriate character
-				output = (char)(0x30 + toConvert) + output;
-			}
+            if (toConvert > 9)
+            {
+                //0x41 is ASCII 'a', value of character will index appropriate letter
+                toConvert -= 0xA;
+                output = (char)(0x61 + toConvert) + output;
+            }
+            else
+            {
+                //0x30 is ASCII '0', value of number will index appropriate character
+                output = (char)(0x30 + toConvert) + output;
+            }
 
-		}
+        }
 
-		return output;
-	}
-	else
-	{
-		return "0";
-	}
+        return output;
+    }
+    else
+    {
+        return "0";
+    }
 }
 
 std::string hexToASCIIU(unsigned short value)
 {
-	std::string output = "";
+    std::string output = "";
 
-	if (value > 0)
-	{
-		while (value > 0)
-		{
-			byte toConvert = value & 0x0F;
-			value >>= 4;
+    if (value > 0)
+    {
+        while (value > 0)
+        {
+            unsigned char toConvert = value & 0x0F;
+            value >>= 4;
 
-			if (toConvert > 9)
-			{
-				//0x41 is ASCII 'a', value of character will index appropriate letter
-				toConvert -= 0xA;
-				output = (char)(0x61 + toConvert) + output;
-			}
-			else
-			{
-				//0x30 is ASCII '0', value of number will index appropriate character
-				output = (char)(0x30 + toConvert) + output;
-			}
+            if (toConvert > 9)
+            {
+                //0x41 is ASCII 'a', value of character will index appropriate letter
+                toConvert -= 0xA;
+                output = (char)(0x61 + toConvert) + output;
+            }
+            else
+            {
+                //0x30 is ASCII '0', value of number will index appropriate character
+                output = (char)(0x30 + toConvert) + output;
+            }
 
-		}
+        }
 
-		return output;
-	}
-	else
-	{
-		return "0";
-	}
+        return output;
+    }
+    else
+    {
+        return "0";
+    }
 }
 
-std::string hexToASCII(byte value)
+std::string hexToASCII(unsigned char value)
 {
-	std::string output = "";
+    std::string output = "";
 
-	if (value > 0)
-	{
-		while (value > 0)
-		{
-			byte toConvert = value & 0x0F;
-			value >>= 4;
+    if (value > 0)
+    {
+        while (value > 0)
+        {
+            unsigned char toConvert = value & 0x0F;
+            value >>= 4;
 
-			if (toConvert > 9)
+            if (toConvert > 9)
 			{
-				//0x41 is ASCII 'a', value of character will index appropriate letter
-				toConvert -= 0xA;
-				output = (char)(0x61 + toConvert) + output;
-			}
-			else
-			{
-				//0x30 is ASCII '0', value of number will index appropriate character
-				output = (char)(0x30 + toConvert) + output;
-			}
+                //0x41 is ASCII 'a', value of character will index appropriate letter
+                toConvert -= 0xA;
+                output = (char)(0x61 + toConvert) + output;
+            }
+            else
+            {
+                //0x30 is ASCII '0', value of number will index appropriate character
+                output = (char)(0x30 + toConvert) + output;
+            }
 
-		}
+        }
 
-		return output;
-	}
-	else
-	{
-		return "0";
-	}
+        return output;
+    }
+    else
+    {
+        return "0";
+    }
 }
--- GameBeak/src/Binary.h
+++ GameBeak/src/Binary.h
@@ -1,19 +1,18 @@
 #pragma once
 
-#include <Windows.h>
 #include <string>
 
 #ifndef BINARYH
 #define BINARYH
 
-byte rotateLeft(byte number);
-byte rotateLeft(byte number, int shiftAmount);
-byte rotateRight(byte number);
-byte rotateRight(byte number, int shiftAmount);
-byte returnHalfNibble(byte data, int halfNibblePosition);
-byte reverseBits(byte number);
+unsigned char rotateLeft(unsigned char number);
+unsigned char rotateLeft(unsigned char number, int shiftAmount);
+unsigned char rotateRight(unsigned char number);
+unsigned char rotateRight(unsigned char number, int shiftAmount);
+unsigned char returnHalfNibble(unsigned char data, int halfNibblePosition);
+unsigned char reverseBits(unsigned char number);
 std::string hexToASCII(int value);
 std::string hexToASCIIU(unsigned short value);
-std::string hexToASCII(byte value);
+std::string hexToASCII(unsigned char value);
 
 #endif
