ac02e8a755c4a7b32bf0c26374f952125a371f6a
Dan McCarthy <dan.willy.mccarthy@gmail.com>
2018-10-07 19:47:39 -0500

    MBC5: Replaced instance of byte w/ unsigned char

--- GameBeak/src/MBC5.h
+++ GameBeak/src/MBC5.h
@@ -3,7 +3,7 @@ class MBC5
 {
 public:
 	void changeMBC5RomBanks(int bankNumber);
-	void writeMBC5Value(short address, byte value);
+        void writeMBC5Value(short address, unsigned char value);
 	MBC5();
 	~MBC5();
 };
