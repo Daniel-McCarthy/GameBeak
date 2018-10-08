ac557718c528ab1194b281a712c652835d09dba5
Dan McCarthy <dan.willy.mccarthy@gmail.com>
2018-10-07 19:44:52 -0500

    MBC3: Replaced instance of byte with unsigned char & fixed formatting

--- GameBeak/src/MBC3.h
+++ GameBeak/src/MBC3.h
@@ -2,9 +2,9 @@
 class MBC3
 {
 public:
-	void changeMBC3RomBanks(int bankNumber);
-	void writeMBC3Value(short address, byte value);
-	MBC3();
-	~MBC3();
+    void changeMBC3RomBanks(int bankNumber);
+    void writeMBC3Value(short address, unsigned char value);
+    MBC3();
+    ~MBC3();
 };
 
