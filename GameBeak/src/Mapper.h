4388eb4babfe5ff9dd3674f1894df5975017166d
Dan McCarthy <dan.willy.mccarthy@gmail.com>
2018-10-07 19:40:18 -0500

    Mapper: Replaced byte instances with unsigned char in header

--- GameBeak/src/Mapper.h
+++ GameBeak/src/Mapper.h
@@ -1,20 +1,16 @@
-#include <Windows.h>
-#include <string>
-using namespace std;
-
 #pragma once
 class Mapper
 {
 public:
-	byte beakExternalRam[0x1E000];
-	bool ramEnabled = false;
-	short romBankNumber = 0;
-	byte ramBankNumber = 0;
-	bool bankingMode = false; //0: Rom 1: Ram
+    unsigned char beakExternalRam[0x1E000];
+    bool ramEnabled = false;
+    short romBankNumber = 0;
+    unsigned char ramBankNumber = 0;
+    bool bankingMode = false; //0: Rom 1: Ram
 
-	void changeRamBanks(int bankNumber);
+    void changeRamBanks(int bankNumber);
 
-	Mapper();
-	~Mapper();
+    Mapper();
+    ~Mapper();
 };
 
