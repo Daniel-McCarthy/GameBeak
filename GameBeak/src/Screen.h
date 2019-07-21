53a7552be27b4edf770285cfdb55490d49e15863
Dan McCarthy <dan.willy.mccarthy@gmail.com>
2019-07-21 17:16:10 -0500

    Added arrays and variables needed for Screen display

--- GameBeak/src/Screen.h
+++ GameBeak/src/Screen.h
@@ -26,8 +26,16 @@ private:
     Memory& memory;
     Gpu& gpu;
 
+    QVector<QColor> bgPixels = QVector<QColor>(256 * 256);
+    QColor windowPixels[256 * 256];
+    QColor spritePixels[256 * 256];
+    QColor tileDebugPixels[256 * 256];
+    unsigned char scrollXValues[256];
     int gpuMode = 1; //BGB defaults this to 1, V-Blank. This should be true.
     int getGPUMode();
+    QImage screen = QImage(160, 144, QImage::Format_RGB32);
+    QImage debugTileScreen;
+    bool poweringOn = true;
 };
 
 #endif // SCREEN_H
