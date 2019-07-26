c9eb64577dfd71e4e5eb0dd5f65a3a6ea13a8473
Dan McCarthy <dan.willy.mccarthy@gmail.com>
2019-07-25 21:19:55 -0500

    Use OpenGL for drawing frames to window
    Previously the QGraphicsView was used for drawing QImages. This worked pretty well for a simple implementation, however came with a lot of overhead and caused poor performance compared to other options.
    
    Instead OpenGL is now being used resulting in better performance with more improvements and optimizations to follow.

--- GameBeak/src/canvas.cpp
+++ GameBeak/src/canvas.cpp
@@ -1,26 +1,127 @@
 #include "canvas.h"
 
 Canvas::Canvas(QWidget* parent)
-    : QGraphicsView(parent)
+    : QOpenGLWidget(parent), m_Texture(), m_indexBuffer(QOpenGLBuffer::IndexBuffer)
 {
-    scene = new QGraphicsScene();
-    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    setInteractive(false);
     setFocusPolicy(Qt::NoFocus);
+    setMouseTracking(false);
 }
 
 Canvas::~Canvas() {
-    delete scene;
+    makeCurrent();
+    delete m_Texture;
+    delete vertexShader;
+    delete fragmentShader;
 }
 
+void Canvas::initializeGL() {
+    makeCurrent();
+    initializeOpenGLFunctions();
+
+    glMatrixMode(GL_PROJECTION);
+
+    vertexShader = new QOpenGLShader(QOpenGLShader::Vertex);
+    const char* vertexShaderSource =
+            "attribute highp vec4 qt_Vertex;\n"
+            "attribute highp vec2 qt_MultiTexCoord0;\n"
+            "uniform highp mat4 qt_ModelViewProjectionMatrix;\n"
+            "varying highp vec2 qt_TexCoord0;\n"
+            "void main(void)\n"
+            "{\n"
+            "    gl_Position = qt_ModelViewProjectionMatrix * qt_Vertex;\n"
+            "    qt_TexCoord0 = qt_MultiTexCoord0;\n"
+            "}\n";
+
+    vertexShader->compileSourceCode(vertexShaderSource);
+
+    fragmentShader = new QOpenGLShader(QOpenGLShader::Fragment);
+    const char* fragmentShaderSource =
+            "uniform sampler2D qt_Texture0;\n"
+            "varying highp vec2 qt_TexCoord0;\n"
+            "void main(void)\n"
+            "{\n"
+            "    gl_FragColor = texture2D(qt_Texture0, qt_TexCoord0);\n"
+            "}\n";
+    fragmentShader->compileSourceCode(fragmentShaderSource);
+
+    shaderProgram.addShader(vertexShader);
+    shaderProgram.addShader(fragmentShader);
+    shaderProgram.link();
+
+    m_Texture = new QOpenGLTexture((currentFrame).mirrored());
+    buildDrawingSurface();
+
+}
+
+void Canvas::paintGL() {
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    QMatrix4x4 modelViewMatrix;
+    modelViewMatrix.setToIdentity();
+    modelViewMatrix.translate(0.0, 0.0, -5.0);
+
+    updateTexture();
+    m_Texture->bind(0);
+
+    shaderProgram.bind();
+    shaderProgram.setUniformValue("qt_ModelViewProjectionMatrix", m_projectionMatrix * modelViewMatrix);
+    shaderProgram.setUniformValue("qt_Texture0", 0);
+
+    m_arrayBuffer.bind();
+
+    int vertLoc = shaderProgram.attributeLocation("qt_Vertex");
+    shaderProgram.enableAttributeArray(vertLoc);
+    shaderProgram.setAttributeBuffer(vertLoc, GL_FLOAT, 0, 3, sizeof(GLfloat) * (3 + 2 + 3));
+
+    int texLoc = shaderProgram.attributeLocation("qt_MultiTexCoord0");
+    shaderProgram.enableAttributeArray(texLoc);
+    shaderProgram.setAttributeBuffer(texLoc, GL_FLOAT, sizeof(GLfloat) * 3, 2, sizeof(GLfloat) * (3 + 2 + 3));
+
+    m_indexBuffer.bind();
+
+    glDrawElements(GL_TRIANGLES, m_indexBuffer.size(), GL_UNSIGNED_INT, 0);
+}
+
+void Canvas::buildDrawingSurface() {
+    GLfloat vertices[] = {
+        // Vertex Positions         // Texture Coordinates      // Normals
+        -2.295f,  2.05f, 0.0f,      0.0f, 1.0f,                 0.0f, 0.0f, 1.0f,
+        -2.295f, -2.05f, 0.0f,      0.0f, 0.0f,                 0.0f, 0.0f, 1.0f,
+         2.295f,  2.05f, 0.0f,      1.0f, 1.0f,                 0.0f, 0.0f, 1.0f,
+         2.295f, -2.05f, 0.0f,      1.0f, 0.0f,                 0.0f, 0.0f, 1.0f,
+    };
+
+    GLuint indices[] = {
+        0, 1, 2,
+        2, 1 ,3
+    };
+
+    m_arrayBuffer.create();
+    m_arrayBuffer.bind();
+    m_arrayBuffer.allocate(vertices, sizeof(vertices));
+    m_arrayBuffer.release();
+
+    m_indexBuffer.create();
+    m_indexBuffer.bind();
+    m_indexBuffer.allocate(indices, sizeof (indices));
+    m_indexBuffer.release();
+}
+
+void Canvas:: updateTexture() {
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, currentFrame.width(), currentFrame.height(), GL_BGRA, GL_UNSIGNED_BYTE, currentFrame.mirrored().bits());
+}
+
+void Canvas::resizeGL(int w, int h) {
+    float aspect = w /(float)h;
+    m_projectionMatrix.setToIdentity();
+    m_projectionMatrix.perspective(45, aspect, 0.1f, 10.0f);
+}
 
 void Canvas::drawImage(QImage image)
 {
     image = image.scaled(320, 288, Qt::IgnoreAspectRatio);
-    scene->clear();
-    QPixmap pixmap = QPixmap::fromImage(image);
-    scene->addPixmap(pixmap);
-    setScene(scene);
-    show();
+    currentFrame = image;
+    update();
 }
+
--- GameBeak/src/canvas.h
+++ GameBeak/src/canvas.h
@@ -3,14 +3,43 @@
 
 #include <QWidget>
 #include <QImage>
-#include <QGraphicsView>
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLBuffer>
+#include <QOpenGLTexture>
+#include <QColor>
+#include <QPainter>
+#include <QMatrix4x4>
 
-class Canvas : public QGraphicsView
+class Canvas : public QOpenGLWidget, protected QOpenGLFunctions
 {
+    Q_OBJECT
 public:
     Canvas(QWidget* parent = 0);
     ~Canvas();
-    QGraphicsScene *scene;
+
+private:
+    void paintTexturedCube();
+    void buildDrawingSurface();
+    void updateTexture();
+    QImage currentFrame;
+
+    QMatrix4x4 m_projectionMatrix;
+    QOpenGLShaderProgram shaderProgram;
+    QOpenGLTexture* m_Texture;
+    QOpenGLBuffer m_arrayBuffer;
+    QOpenGLBuffer m_indexBuffer;
+    QOpenGLShader* vertexShader;
+    QOpenGLShader* fragmentShader;
+    QOpenGLBuffer vertexBuffer;
+    QMatrix4x4 modelViewMatrix;
+
+protected:
+    void initializeGL() override;
+    void paintGL() override;
+    void resizeGL(int width, int height) override;
+
 
 public slots:
     void drawImage(QImage image);
